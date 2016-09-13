f97c2ed8025a9f5b7203130dd2b2e80d310e4337
xiaochang <qianchang.xiao@gmail.com>
2016-09-14 01:57:31 +0800

    1.完成卫星菜单；

--- SateliteMenuDemo/app/src/main/java/com/changxiao/satelitemenudemo/MainActivity.java
+++ SateliteMenuDemo/app/src/main/java/com/changxiao/satelitemenudemo/MainActivity.java
@@ -2,12 +2,69 @@ package com.changxiao.satelitemenudemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.changxiao.satelitemenudemo.widget.SateliteMenu;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.sateliteMenu)
+    SateliteMenu sateliteMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initView();
+    }
+
+    private void initView() {
+        sateliteMenu.setOnMenuItemClickListener(new SateliteMenu.onMenuItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                // 可以使用标记来判断点击的Viwew，当然必须给ChildViewItem设置了Tag
+                if ("1".equals(view.getTag())) {
+                    Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                } else if ("2".equals(view.getTag())) {
+                    Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                } else if ("3".equals(view.getTag())) {
+                    Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                } else if ("4".equals(view.getTag())) {
+                    Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                } else if ("5".equals(view.getTag())) {
+                    Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                } else if ("6".equals(view.getTag())) {
+                    Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                }
+
+                //可以使用position来判断
+                switch (position) {
+                    case 1:
+                        Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                        break;
+                    case 4:
+                        Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                        break;
+                    case 5:
+                        Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                        break;
+                    case 6:
+                        Toast.makeText(getApplicationContext(), "点击了" + position, Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 }
