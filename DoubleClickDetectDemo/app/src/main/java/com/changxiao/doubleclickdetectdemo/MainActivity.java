ab478f85a1fe513ea3749883ab865909ad955983
xiaochang <qianchang.xiao@gmail.com>
2016-12-05 16:28:53 +0800

    添加注释；

--- DoubleClickDetectDemo/app/src/main/java/com/changxiao/doubleclickdetectdemo/MainActivity.java
+++ DoubleClickDetectDemo/app/src/main/java/com/changxiao/doubleclickdetectdemo/MainActivity.java
@@ -37,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * 操作符：debounce仅在过了一段指定的时间还没发射数据时才发射一个数据(
+     * 用简单的话讲就是当N个结点发生的时间太靠近（即发生的时间差小于设定的值T），debounce就会自动过滤掉前N-1个结点。)
+     *
      * 这里rx的双击检测会在debounce 1000毫秒之后再才执行call方法(也就是间隔debounce执行)
      *
      * 总结：这里要做到双击检测不适合，感觉有延迟的效果。
@@ -45,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void doubleClickDetect(View view) {
         Observable<Void> observable = RxView.clicks(view).share();
-        observable.buffer(observable.debounce(1000, TimeUnit.MILLISECONDS))
+        observable.buffer(observable.debounce(1, TimeUnit.SECONDS))
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action1<List<Void>>() {
                     @Override
