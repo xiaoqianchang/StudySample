08bea5d903547a243f45a5faad35b7297ff7af04
xiaochang <qianchang.xiao@gmail.com>
2021-05-21 13:37:57 +0800

    feat: Update SplashActivity.

--- WiFiAnalyzerDemo/app/src/main/java/com/chang/android/wifi/ui/activity/SplashActivity.kt
+++ WiFiAnalyzerDemo/app/src/main/java/com/chang/android/wifi/ui/activity/SplashActivity.kt
@@ -1,12 +1,13 @@
 package com.chang.android.wifi.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.chang.android.wifi.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
